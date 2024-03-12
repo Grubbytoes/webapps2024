9740759a6a0a3ab078be2cc3f61f74f4a129b62a
Hugo <haaa20@sussex.ac.uk>
2024-03-12 20:19:31 +0000

    fucking around with forms

--- register/views.py
+++ register/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from . import forms
 
-# Create your views here.
+
+def new_user(request):
+    context = {'page_title': 'register', 'form': forms.RegisterForm()}
+    return render(request, 'register/new_user.html', context)
