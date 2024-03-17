6016f90c41105e3445e9ea3b037d89b1eddfa013
hughie <=>
2024-03-17 13:51:22 +0000

    passwords now hash upon user creation

--- globaltools/models.py
+++ globaltools/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -16,10 +17,12 @@ class UserAccount(models.Model):
         if UserAccount.objects.filter(username=new_username).exists():
             return False
 
-        new_account: UserAccount = UserAccount(username=new_username, bad_password=new_password, email=new_email)
+        new_account: UserAccount = UserAccount(username=new_username, email=new_email)
+        new_account.bad_password = make_password(new_password)
         new_account.holding = Holding(account=new_account, balance=1000)
         new_account.save()
         return True
+
 
 class Holding(models.Model):
     """
@@ -29,7 +32,7 @@ class Holding(models.Model):
     balance = models.PositiveIntegerField()
 
 
-## STATIC FUNCTION
+# STATIC FUNCTION
 
 def account_factory(new_username: str, new_password: str) -> UserAccount:
     """
