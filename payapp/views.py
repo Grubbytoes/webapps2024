3464203c920344b8a106a8bfe23766046908f8d7
hughie <=>
2024-04-10 21:16:34 +0100

    HA HA IT'S WORKING

--- payapp/views.py
+++ payapp/views.py
@@ -1,15 +1,29 @@
 from django.contrib.auth import authenticate
 from django.shortcuts import render
-from . import forms
+from django.views.decorators.csrf import csrf_exempt
+
+from . import forms, models
+
 
 # Create your views here.
+@csrf_exempt
 def login(request):
     if request.method == "POST":
+        # If the form is invalid, exit.
+        # Otherwise, get the data
         posted_form = forms.LoginForm(request.POST)
+        if not posted_form.is_valid():
+            return
         posted_form_data = posted_form.cleaned_data
-        logged_in_user = authenticate(posted_form_data['username'], posted_form_data['password'])
 
-        if logged_in_user is not None:
-            print("YOU'RE IN!!")
+        # Try and find a user of the right name, and authenticate their password
+        query = models.UserAccount.objects.filter(username=posted_form_data['username'])
+        if query is None:
+            return
+        user_by_name: models.UserAccount = query[0]
+        if not user_by_name.check_password(posted_form_data['password']):
+            return
+
+        print('beep')
     else:
         return render(request, 'login.html', {'page_title': 'login', 'form': forms.LoginForm()})
