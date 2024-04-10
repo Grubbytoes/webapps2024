46b6bb7918a04a91d2e59d40e4893b682231fea5
hughie <=>
2024-04-10 21:27:34 +0100

    Made 'register' app

--- webapps2024/settings.py
+++ webapps2024/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "payapp.apps.PayappConfig"
+    "payapp.apps.PayappConfig",
+    "register.apps.RegisterConfig"
 ]
 
 MIDDLEWARE = [
