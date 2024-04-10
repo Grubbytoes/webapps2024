b1af193ab24a6775820d130b9d1b7ce976e43419
hughie <=>
2024-04-10 20:08:46 +0100

    bacon is being made

--- payapp/admin.py
+++ payapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from payapp import models
 
 # Register your models here.
+admin.site.register(models.UserAccount, UserAdmin)
