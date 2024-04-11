7a975ed57065cbabb41e0ad02dccf0ab7ccd88c1
hughie <=>
2024-04-11 12:48:07 +0100

    Now creates account properly! AND adds a holding!!
    Don't get too exited, tho. Want to do verification that the username is unique etc.

--- payapp/admin.py
+++ payapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 from django.contrib.auth.admin import UserAdmin
 from payapp import models
 
 # Register your models here.
 admin.site.register(models.UserAccount, UserAdmin)
+admin.site.register(models.Holding, ModelAdmin)
--- register/views.py
+++ register/views.py
@@ -6,27 +6,39 @@ from payapp import models
 def register(request):
     # The variables we will use
     logged_in_ok = False
-    posted_form = None
 
-    # Are they getting the form, or positing a completed form?
+    # Internal methods we will use
+    def create_new_user(form_) -> bool:
+        # Get the data from the form
+        data = form_.cleaned_data
+
+        # Creating and saving the user
+        new_user = models.UserAccount(
+            username=data['username'],
+            email=data['email'],
+            first_name= data['first_name'],
+            last_name=data['last_name']
+        )
+        new_user.set_password(raw_password=data['password'])
+        new_user.save()
+
+        # Create the new Holding
+        new_holding = models.Holding(balance=1000, account=new_user)
+        new_holding.save()
+
+        return True
+
+    # Are they positing a completed form?
     if request.method == 'POST':
         posted_form = forms.RegisterForm(request.POST)
         if posted_form.is_valid():
-            # Get the data from the form
-            data = posted_form.cleaned_data
-            new_username = data['username']
-            new_raw_password = data['password']
-
-            # Creating and saving the user
-            new_user = models.UserAccount(username=new_username)
-            new_user.set_password(raw_password=new_raw_password)
-            new_user.save()
+            logged_in_ok = create_new_user(posted_form)
 
 
-    else: # Safe to assume == 'GET'
+    # Have they logged in?
+    if logged_in_ok:
         pass
-
-
-    return render(request, 'register.html', {
-        'page_title': 'register new user', 'form': forms.RegisterForm()
-    })
+    else:
+        return render(request, 'register.html', {
+            'page_title': 'register new user', 'form': forms.RegisterForm()
+        })
